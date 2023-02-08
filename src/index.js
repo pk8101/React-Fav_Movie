4ba073af8de94b4497d533dc6f6e36acdaa2e072
pk8101 <pradeepvirat913@gmail.com>
2023-02-08 16:41:53 +0530

    store added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {legacy_createStore as createStore} from 'redux'
 import './index.css';
-import App from './components/App';
+import App from './components/App'; 
+import movies from './reducers';
 
 
+const store = createStore(movies);
+console.log("store",store)
+console.log("store",store.getState())
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,7 @@
+
+
+
+
 export default function movies(state=[],action){
     if(action.type==='ADD_MOVIE'){
         return action.movies;
